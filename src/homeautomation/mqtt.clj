c70aca3b0731244350c1e4e915f8bba0e58fda8a
Harald Koch <chk@cfrq.net>
2015-09-05 17:13:11 -0400

    add a JSON writer for java Date

--- src/homeautomation/mqtt.clj
+++ src/homeautomation/mqtt.clj
@@ -6,7 +6,13 @@
             [taoensso.timbre :as timbre]
             [clj-time.core :as t]
             [clj-time.coerce :as c]
-            [clj-time.format :as f]))
+            [clj-time.format :as f])
+  (:import [java.util Date]))
+
+(defn- write-date [x ^PrintWriter out]
+  (.print out (str x)))
+
+(extend java.util.Date json/JSONWriter {:-write write-date})
 
 (defonce conn (atom nil))
 
