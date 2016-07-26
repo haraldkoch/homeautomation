25e755d8fb69c6c89cdb504de91c8681470d3e5b
Harald Koch <Harald.Koch@UHN.CA>
2016-07-26 17:27:51 -0400

    convert all web handlers to use the response handler macro, and to use deconstruction.

--- src/clj/homeautomation/routes/home.clj
+++ src/clj/homeautomation/routes/home.clj
@@ -21,70 +21,52 @@
          (response/internal-server-error {:error (.getMessage e#)})))))
 
 (response-handler get-users []
-                  (db/get-users))
+  (db/get-users))
 
-(defn add-user [request]
-  (try
-    (do (db/create-user! (:params request))
-        (let [message (str "user " (get-in request [:params :username]) " created successfully")]
-          (log/info message)
-          message))
-    (catch Exception e#
-      (log/error e# "error handling request")
-      (response/internal-server-error {:error (.getMessage e#)}))))
+(response-handler add-user [{:keys [params]}]
+  (do (db/create-user! params)
+      (let [message (str "user " (:username params) " created successfully")]
+        (log/info message)
+        message)))
 
-(defn set-device-owner [request]
-  (try
-    (let [current-owner (:owner (db/get-device {:id (get-in request [:params :device_id])}))
-          message (str "device now owned by " (get-in request [:params :owner]))]
-      (db/set-device-owner! (:params request))
-      (presence/update-user-presence (get-in request [:params :owner])) ;; new owner
-      (presence/update-user-presence current-owner)
-      (log/info message)
-      message)
-    (catch Exception e#
-      (log/error e# "error handling request")
-      (response/internal-server-error {:error (.getMessage e#)}))))
+(response-handler set-device-owner [{{:keys [device_id owner] :as params} :params}]
+  (let [current-owner (:owner (db/get-device {:id device_id}))
+        message (str "changed owner from " current-owner " to " owner)]
+    (db/set-device-owner! params)
+    (presence/update-user-presence owner)                   ;; new owner
+    (presence/update-user-presence current-owner)
+    (log/info message)
+    message))
 
-(defn set-device-ignore [request]
-  (try
-    (do (db/set-device-ignore! (:params request))
-        (let [ignored? (get-in request [:params :ignore])
-              device-id (get-in request [:params :device_id])
-              device (db/get-device {:id device-id})
-              message (str "device " {:name device} " is now " (if ignored? "ignored" "NOT ignored"))]
-          (log/info message)
-          (presence/update-user-presence (:owner device))
-          message))
-    (catch Exception e#
-      (log/error e# "error handling request")
-      (response/internal-server-error {:error (.getMessage e#)}))))
+(response-handler set-device-ignore [{{:keys [ignore device_id] :as params} :params}]
+  (do (db/set-device-ignore! params)
+      (let [device (db/get-device {:id device_id})
+            message (str "device " {:name device} " is now " (if ignore "ignored" "NOT ignored"))]
+        (log/info message)
+        (presence/update-user-presence (:owner device))
+        message)))
 
 ;; FIXME: use a validator instead - see Dmitri's selme-guestbook
-(defn set-device-name [request]
-  (try
-    (let [new-name (get-in request [:params :name])
-          message (str "device name changed to '" new-name "'")]
-      (if-not (clojure.string/blank? new-name)
-        (do (db/set-device-name! (:params request))
-            (log/info message)
-            message)
-        (do (log/error "set-device-name: new name cannot be empty")
-            (response/internal-server-error {:error "set-device-name: new name cannot be empty"}))))
-    (catch Exception e#
-      (log/error e# "error handling request")
-      (response/internal-server-error {:error (.getMessage e#)}))))
+(response-handler set-device-name [{{:keys [device_id name] :as params} :params}]
+  (let [device (db/get-device {:id device_id})
+        old-name (:name device)
+        message (str "device name changed from '" old-name "' to '" name "'")]
+    (if-not (clojure.string/blank? name)
+      (do (db/set-device-name! params)
+          (log/info message)
+          message)
+      (do (log/error "set-device-name: new name cannot be empty")
+          (response/internal-server-error {:error "set-device-name: new name cannot be empty"})))))
 
 (response-handler get-devices []
-                  (db/get-devices))
+  (db/get-devices))
 
 (defroutes home-routes
-           (GET "/" [] (home-page))
-           (GET "/users" [] (get-users))
-           (GET "/devices" [] (get-devices))
-           (POST "/add-user" request (add-user request))
-           (POST "/set-device-owner" request (set-device-owner request))
-           (POST "/set-device-name" request (set-device-name request))
-           (POST "/set-device-ignore" request (set-device-ignore request))
-           (GET "/docs" [] (response/ok (-> "docs/docs.md" io/resource slurp))))
-
+  (GET "/" [] (home-page))
+  (GET "/users" [] (get-users))
+  (GET "/devices" [] (get-devices))
+  (POST "/add-user" request (add-user request))
+  (POST "/set-device-owner" request (set-device-owner request))
+  (POST "/set-device-name" request (set-device-name request))
+  (POST "/set-device-ignore" request (set-device-ignore request))
+  (GET "/docs" [] (response/ok (-> "docs/docs.md" io/resource slurp))))
